e06bbb050d2a5c6f56cbc24dbd503d4c9e9add80
Sonu Sindhu <sonusindhu10@yahoo.com>
2016-12-09 16:11:13 +0530

    added route and controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        die("home");
+    }
+    public function about(){
+        die("about");
+    }
+    public function contact(){
+        die("contact");
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'HomeController@index');
+Route::get('/about', 'HomeController@about');
+Route::get('/contact', 'HomeController@contact');
+
